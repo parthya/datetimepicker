41f83682e4be5b672444c9069424173e42805272
Parth Kulkarni <parthya7@gmail.com>
2020-03-11 20:37:45 +0530

    Update datepicker.android.js

--- src/datepicker.android.js
+++ src/datepicker.android.js
@@ -39,7 +39,7 @@ export default class DatePickerAndroid {
     toMilliseconds(options, 'value', 'minimumDate', 'maximumDate');
     options.display = options.display || DISPLAY_DEFAULT;
 
-    return NativeModules.RNDatePickerAndroid.open(options);
+    return NativeModules.DatePickerAndroid.open(options);
   }
 
   /**
